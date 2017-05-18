04ff0222884159c073fbd7e71c937f19e70b24ce
Kélian Clerc <kelian.clerc@applidium.com>
2017-05-18 10:58:31 +0200

    Implement method sending request to demo
    Change-Id: I5a0ba6236b2feb6ac8a278cfe34f296746bc79e6

--- app/src/main/java/com/applidium/graphql/client/data/net/ServiceGraphQLRepository.java
+++ app/src/main/java/com/applidium/graphql/client/data/net/ServiceGraphQLRepository.java
@@ -1,17 +1,24 @@
 package com.applidium.graphql.client.data.net;
 
 import com.applidium.graphql.client.core.boundary.GraphQLRepository;
+import com.applidium.graphqlient.GraphQL;
+
 import java.io.IOException;
 
 import javax.inject.Inject;
 
+import retrofit2.http.HEAD;
+
 public class ServiceGraphQLRepository implements GraphQLRepository {
 
+    private final GraphQL graphQL;
+
     @Inject ServiceGraphQLRepository() {
+        graphQL = new GraphQL();
     }
 
     @Override
     public String getStringResponse(String request) throws IOException {
-        return "";
+        return graphQL.sendRequest(request);
     }
 }
