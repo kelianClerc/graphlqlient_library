7ae884cecff25592d6f2106aa11b8b76224296ed
Kélian Clerc <kelian.clerc@applidium.com>
2017-05-19 14:48:29 +0200

    Update node and leaf to match super constructor
    Change-Id: Ia8b3ea6288aa22611301acbb7a67b140e8b0533a

--- graphqlient/src/main/java/com/applidium/graphqlient/tree/QLLeaf.java
+++ graphqlient/src/main/java/com/applidium/graphqlient/tree/QLLeaf.java
@@ -1,8 +1,20 @@
 package com.applidium.graphqlient.tree;
 
+import java.util.Map;
+
 public class QLLeaf extends QLElement {
 
     public QLLeaf(String name) {
         super(name);
+    }
+
+    public QLLeaf(String name, String alias) {
+        super(name, alias);
+    }
+    public QLLeaf(String name, Map<String, Object> params) {
+        super(name, params);
+    }
+    public QLLeaf(String name, String alias, Map<String, Object> params) {
+        super(name, alias, params);
     }
 }
