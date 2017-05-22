7849d09ace4887cad9fb55e62cb7b302a677ef53
Kélian Clerc <kelian.clerc@applidium.com>
2017-05-22 11:05:54 +0200

    Implement demo to create request from object
    Change-Id: I8e5fcb2bd5a4ae5e5b2f70c1ba5925ce409eab44

---
+++ app/src/main/java/com/applidium/graphql/client/data/net/graphql/model/Endpoint.java
@@ -0,0 +1,38 @@
+package com.applidium.graphql.client.data.net.graphql.model;
+
+import com.applidium.graphqlient.annotations.Argument;
+import com.applidium.graphqlient.annotations.Parameters;
+import com.applidium.graphqlient.model.QLModel;
+import com.applidium.graphqlient.tree.QLElement;
+import com.applidium.graphqlient.tree.QLNode;
+
+import java.util.List;
+
+public class Endpoint extends QLModel {
+    @Parameters(table={
+        @Argument(argumentName = "id", argumentValue = "1")
+    })
+    private User user;
+    private List<User> users;
+
+    public Endpoint() {
+    }
+
+    public QLNode getUser() {
+        try {
+            return createNodeFromField(getClass().getDeclaredField("user"));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public QLElement getUsers() {
+        try {
+            return createNodeFromField(getClass().getDeclaredField("users"));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/applidium/graphql/client/data/net/graphql/model/Post.java
@@ -0,0 +1,8 @@
+package com.applidium.graphql.client.data.net.graphql.model;
+
+import com.applidium.graphqlient.model.QLModel;
+
+class Post extends QLModel {
+    private String id;
+    private String title;
+}
