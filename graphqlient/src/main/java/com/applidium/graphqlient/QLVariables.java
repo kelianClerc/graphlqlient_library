edbfa7b74cd73001074d1be20597f8e00c0c44b1
Kélian Clerc <kelian.clerc@applidium.com>
2017-05-23 16:04:38 +0200

    Add variable field in http query and object to build it + test
    Change-Id: Iab9900522f8daa39fbadefabf7904769037fef71

---
+++ graphqlient/src/main/java/com/applidium/graphqlient/QLVariables.java
@@ -0,0 +1,44 @@
+package com.applidium.graphqlient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QLVariables {
+    public static final String SEPARATOR = ",";
+    private static final String OPENING_CHARACTER = "{";
+    private static final String CLOSING_CHARACTER = "}";
+    Map<String, Object> parameters;
+
+    public QLVariables() {
+        parameters = new HashMap<>();
+    }
+
+    public QLVariables(Map<String, Object> parameters) {
+        this.parameters = parameters;
+    }
+
+    public Map<String, Object> getParameters() {
+        return parameters;
+    }
+
+    public String print() {
+        String res = "";
+        int i = 0;
+        res += OPENING_CHARACTER;
+        for (String key : parameters.keySet()) {
+            res += "\"" + key + "\":";
+            Object o = parameters.get(key);
+            if (o instanceof String) {
+                res += "\"" + String.valueOf(o) + "\"";
+            } else {
+                res += String.valueOf(o);
+            }
+            if (i < parameters.keySet().size() - 1) {
+                res += SEPARATOR;
+            }
+            i++;
+        }
+        res += CLOSING_CHARACTER;
+        return res;
+    }
+}
