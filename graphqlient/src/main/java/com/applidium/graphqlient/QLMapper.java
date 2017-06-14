d495e6ab177ed1862cbffb65a1847a24b1f3355f
Kélian Clerc <kelian.clerc@applidium.com>
2017-06-14 12:04:38 +0200

    Parse trimmed string to object instead of string directly
    Change-Id: I19fa9b3224491eecb4ceaa788ff35fe732a021c6

--- graphqlient/src/main/java/com/applidium/graphqlient/QLMapper.java
+++ graphqlient/src/main/java/com/applidium/graphqlient/QLMapper.java
@@ -7,8 +7,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import io.norberg.automatter.gson.AutoMatterTypeAdapterFactory;
 import okhttp3.ResponseBody;
@@ -21,15 +19,15 @@ public class QLMapper {
 
     public QLResponse convert(ResponseBody body, QLRequest query) throws IOException {
 
-        Map responseMap = new HashMap();
-        JsonObject data = trimResponse(body);
+        String responseBody = body.string();
+        JsonObject data = trimResponse(responseBody);
         String result = gson.toJson(data);
-        QLResponseModel response = gson.fromJson(body.string(), query.target());
+        QLResponseModel response = gson.fromJson(data, query.target());
         return new QLResponse(result, response);
     }
 
-    private JsonObject trimResponse(ResponseBody body) throws IOException {
-        JsonElement jsonElement = gson.fromJson(body.string(), JsonElement.class);
+    private JsonObject trimResponse(String body) throws IOException {
+        JsonElement jsonElement = gson.fromJson(body, JsonElement.class);
         return jsonElement.getAsJsonObject().get("data").getAsJsonObject();
     }
 }
